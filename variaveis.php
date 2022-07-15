d9855657f7c7177f146cf78c0550b5d1869e0219
Joaquim Martins Scavone <joaquim.scavone@ifto.edu.br>
2022-07-15 14:43:54 -0300

    Aula 02

---
+++ variaveis.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+por bloco 
+*/
+
+
+//por linha é assim
+
+
+$nome_cliente = $_GET["nome"];
+$cargo_cliente = $_GET['cargo'];
+
+$inteiro = 1;
+$logica  = true;//false;
+$real = 1.8;
+$texto = "Joaquim";
+//echo "<h1> Bem vindo! $nome_cliente ( $cargo_cliente )";
+
+echo $real;
+echo "<br/>";
+$real = 8.5;
+
+echo $real;
