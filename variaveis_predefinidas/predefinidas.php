9373ff55e8c6d77a11b92ef4e90bfd644187695d
Joaquim Martins Scavone <joaquim.scavone@ifto.edu.br>
2022-07-21 01:15:57 -0300

    Aula 22

--- variaveis_predefinidas/predefinidas.php
+++ variaveis_predefinidas/predefinidas.php
@@ -1,15 +1,14 @@
 <?php
 
-$teste = "azul";
+// $teste = "azul";
+// session_start();
+// $_SESSION['usuario'] = 'joaquim';
+myprint($_COOKIE);
 
-myprint($_REQUEST);
-
-
+setcookie('nome','Samuel',time()+60*60*24*30);
 
 
 function myprint($var){
-    global $teste;
-    echo $teste;
     echo "<pre>";
     print_r($var);
     echo "</pre>";
